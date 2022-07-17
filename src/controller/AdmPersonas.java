bf48a30f5f14c91acaba27d34f6967666d5ad820
DaniloPV18 <damipive1809@gmail.com>
2022-07-17 00:06:11 -0500

    AdmPersonas

--- src/controller/AdmPersonas.java
+++ src/controller/AdmPersonas.java
@@ -7,6 +7,10 @@ package controller;
 
 import connection.Conexion;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import model.Persona;
+import utilities.Conversiones;
 
 /**
  *
@@ -15,13 +19,62 @@ import java.sql.Connection;
 public class AdmPersonas {
 
     private static final Connection cn = Conexion.getConnection();
-
-    public AdmPersonas() {
-        System.out.println(getCn());
-    }
-
+    
+    private static final String INSERTAR    =  "  INSERT INTO PERSONA ("
+                                            + "     identificador, nombres, apellidos , fecha_nac, email, estado, fecha_reg, "
+                                            + "     tipoidentificacion_id_tipoid, sexo_id_sexo, nacionalidad_id_nacionalidad "
+                                            + ")VALUES(?,?,?,?,?,?,?,?,?,?)";
+    
+    private static final String ACTUALIZAR  =  " UPDATE PERSONA "
+                                            + "SET "
+                                            + "     identificador = ?, nombres = ?, apellidos = ?, fecha_nac = ?, email = ?, "
+                                            + "     tipoidentificacion_id_tipoid = ?, sexo_id_sexo = ?, nacionalidad_id_nacionalidad = ?"
+                                            + "WHERE id_persona = ? ";
+    
     public static Connection getCn() {
         return cn;
     }
 
+    public void insertar(Persona persona) {
+        getCn();
+        if (cn != null) {
+            try {
+                PreparedStatement ps = cn.prepareStatement(INSERTAR);
+                ps.setString(1, persona.getIdentificador());
+                ps.setString(2, persona.getNombres());
+                ps.setString(3, persona.getApellidos());
+                ps.setDate  (4, Conversiones.getFecha(Conversiones.getFecha(persona.getFechaNac())));
+                ps.setString(5, persona.getEmail());
+                ps.setString(6, persona.getEstado());
+                ps.setDate  (7, Conversiones.getFecha(Conversiones.getFecha(persona.getFechaReg())));
+                ps.setInt   (8, persona.getTipoId().getId());
+                ps.setInt   (9, persona.getSexo().getId());
+                ps.setInt   (10, persona.getNacionalidad().getId());
+                ps.execute();
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
+        }
+    }
+    
+    public void actualizar(int id, Persona persona) {
+        getCn();
+        if (cn != null) {
+            try {
+                PreparedStatement ps = cn.prepareStatement(ACTUALIZAR);
+                ps.setString(1, persona.getIdentificador());
+                ps.setString(2, persona.getNombres());
+                ps.setString(3, persona.getApellidos());
+                ps.setDate  (4, Conversiones.getFecha(Conversiones.getFecha(persona.getFechaNac())));
+                ps.setString(5, persona.getEmail());
+                ps.setInt   (6, persona.getTipoId().getId());
+                ps.setInt   (7, persona.getSexo().getId());
+                ps.setInt   (8, persona.getNacionalidad().getId());
+                ps.setInt   (9, id);
+                ps.execute();
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
+        }
+    }
 }
