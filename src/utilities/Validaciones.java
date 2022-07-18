0cb0fa0952ee5da031b7ad7f9a9c72b800c2ac31
DaniloPV18 <damipive1809@gmail.com>
2022-07-18 17:20:25 -0500

    Test Escenarios y Validaciones

--- src/utilities/Validaciones.java
+++ src/utilities/Validaciones.java
@@ -5,13 +5,14 @@
  */
 package utilities;
 
+import model.Persona;
+
 /**
  *
  * @author N1L0XD
  */
 public class Validaciones {
 
-    
     public static boolean vInt(String cadena) {
         try {
             int num = Integer.parseInt(cadena);
@@ -22,19 +23,44 @@ public class Validaciones {
         }
     }
 
+    public static boolean vInt(int cadena) {
+        try {
+            int num = cadena;
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error: la cadena debe contener solo números.");
+            return false;
+        }
+    }
+
     public static boolean vWords(String cadena) {
-        for (int x = 0; x < cadena.length(); x++) {
-            char c = cadena.charAt(x);
-            // Si no está entre a y z, ni entre A y Z, ni es un espacio
-            if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == ' ')) {
-                System.out.println("Error: la cadena tiene numeros.");
-                return false;
+        if (!cadena.isBlank()) {
+            for (int x = 0; x < cadena.length(); x++) {
+                char c = cadena.charAt(x);
+                // Si no está entre a y z, ni entre A y Z, ni es un espacio
+                if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == ' ')) {
+                    System.out.println("Error: la cadena tiene numeros.");
+                    return false;
+                }
             }
+        }else{
+            System.out.println("Error: la cadena esta vacia.");
+            return false;
         }
         return true;
     }
 
     public static boolean vEdad(long edad) {
-        return (edad >= 18)? true: false;
+        return (edad >= 18) ? true : false;
     }
+
+    public static boolean vPersona(Persona persona) {
+        boolean nombres = vWords(persona.getNombres());
+        boolean apellidos = vWords(persona.getApellidos());
+        boolean tipoId = vInt(persona.getTipoId());
+        boolean sexoId = vInt(persona.getSexo());
+        boolean mayorEdad = vEdad(Conversiones.getEdad(persona.getFechaNac()));
+        return nombres && apellidos && tipoId && sexoId && mayorEdad;
+    }
+
 }
--- test/TestUtilities/TestValidaciones.java
+++ test/TestUtilities/TestValidaciones.java
@@ -5,6 +5,8 @@
  */
 package TestUtilities;
 
+import java.util.Date;
+import model.Persona;
 import utilities.Validaciones;
 /**
  *
@@ -21,5 +23,31 @@ public class TestValidaciones {
         // Comprobar si es mayor de edad
         System.out.println(Validaciones.vEdad(19));
         System.out.println(Validaciones.vEdad(15));
+        
+        //Comprobar datos de objeto Persona -> ESCENARIO EDAD MENOR DE EDAD
+        System.out.println("Comprobar datos de objeto Persona -> ESCENARIO EDAD MENOR DE EDAD");
+        System.out.println(new Persona("0931600548", "IVONNE ", "MINCHALA ", new Date(), "IVONNE@GMAIL.COM", "HABILITADO", 0, 0, 0).toString());
+        System.out.println(Validaciones.vPersona(new Persona("0931600548", "IVONNE ", "MINCHALA ", new Date(), "IVONNE@GMAIL.COM", "HABILITADO", 0, 0, 0)));        
+        
+        //Comprobar datos de objeto Persona -> ESCENARIO EDAD MAYOR DE EDAD
+        System.out.println("Comprobar datos de objeto Persona -> ESCENARIO EDAD MAYOR DE EDAD");
+        System.out.println(new Persona("0931600548", "DANILO ", "PIN ", new java.util.GregorianCalendar(2000, java.util.Calendar.FEBRUARY, 11).getTime(), "DANILO@GMAIL.COM", "HABILITADO", 0, 0, 0).toString());
+        System.out.println(Validaciones.vPersona(new Persona("0931600548", "DANILO ", "PIN ", new java.util.GregorianCalendar(2000, java.util.Calendar.FEBRUARY, 11).getTime(), "DANILO@GMAIL.COM", "HABILITADO", 0, 0, 0)));
+        
+        //Comprobar datos de objeto Persona -> ESCENARIO NOMBRES O APELLIDOS CONTIENEN NUMEROS
+        System.out.println("Comprobar datos de objeto Persona -> ESCENARIO NOMBRES O APELLIDOS CONTIENEN NUMEROS");
+        System.out.println(new Persona("0931600548", "DANILO123 12313", "PIN ", new java.util.GregorianCalendar(2000, java.util.Calendar.FEBRUARY, 11).getTime(), "DANILO@GMAIL.COM", "HABILITADO", 0, 0, 0).toString());
+        System.out.println(Validaciones.vPersona(new Persona("0931600548", "DANILO123 12313", "PIN ", new java.util.GregorianCalendar(2000, java.util.Calendar.FEBRUARY, 11).getTime(), "DANILO@GMAIL.COM", "HABILITADO", 0, 0, 0)));
+        
+        System.out.println(new Persona("0931600548", "DANILO ", "11111 1111231", new java.util.GregorianCalendar(2000, java.util.Calendar.FEBRUARY, 11).getTime(), "DANILO@GMAIL.COM", "HABILITADO", 0, 0, 0).toString());
+        System.out.println(Validaciones.vPersona(new Persona("0931600548", "DANILO ", "11111 1111231", new java.util.GregorianCalendar(2000, java.util.Calendar.FEBRUARY, 11).getTime(), "DANILO@GMAIL.COM", "HABILITADO", 0, 0, 0)));
+        
+        System.out.println(new Persona("0931600548", "", "11111 1111231", new java.util.GregorianCalendar(2000, java.util.Calendar.FEBRUARY, 11).getTime(), "DANILO@GMAIL.COM", "HABILITADO", 0, 0, 0).toString());
+        System.out.println(Validaciones.vPersona(new Persona("0931600548", "", "11111 1111231", new java.util.GregorianCalendar(2000, java.util.Calendar.FEBRUARY, 11).getTime(), "DANILO@GMAIL.COM", "HABILITADO", 0, 0, 0)));
+        
+        //Comprobar datos de objeto Persona -> ESCENARIO LLAVES DE OBJETO CONTIENEN LETRAS O CARACTERES NO ADMITIDOS
+        System.out.println("Comprobar datos de objeto Persona -> ESCENARIO LLAVES DE OBJETO CONTIENEN LETRAS O CARACTERES NO ADMITIDOS ");
+        System.out.println(new Persona("0931600548", "KENETH ", "RIERA", new java.util.GregorianCalendar(2000, java.util.Calendar.FEBRUARY, 11).getTime(), "KENETH@GMAIL.COM", "HABILITADO", 1, 0, 1).toString());
+        System.out.println(Validaciones.vPersona(new Persona("0931600548", "KENETH ", "RIERA", new java.util.GregorianCalendar(2000, java.util.Calendar.FEBRUARY, 11).getTime(), "KENETH@GMAIL.COM", "HABILITADO", 1, 0, 1)));
     }
 }
