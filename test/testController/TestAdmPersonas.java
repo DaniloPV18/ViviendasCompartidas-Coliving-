a29b8ffeffb3f70e79525391b0b5fe6c63399d1f
DaniloPV18 <damipive1809@gmail.com>
2022-07-17 19:27:26 -0500

    Clase Test crud personas con BD

--- test/testController/TestAdmPersonas.java
+++ test/testController/TestAdmPersonas.java
@@ -5,12 +5,38 @@
  */
 package testController;
 
+import controller.AdmPersonasDataBase;
+import java.util.ArrayList;
+import java.util.Date;
+import model.Persona;
+
 /**
  *
  * @author N1L0XD
  */
 public class TestAdmPersonas {
     public static void main(String[] args) {
+        AdmPersonasDataBase.insertar(new Persona("0931600548", "ANGEL PALOMINO", "PIGUAVE GUAGUA", new Date(), "ANGEL@GMAIL.COM", "HABILITADO", 1, 1, 1));
+            
+        ArrayList<Persona> ls1 = AdmPersonasDataBase.consultar();
+        if(ls1!=null && ls1.size()>0){
+            for (Persona persona : ls1) {
+                System.out.println(persona.toString());
+            }
+        }else{
+            System.out.println("lista nula");
+        }        
+        AdmPersonasDataBase.actualizar(44, new Persona("1561651651", "DANILO MIGUEL", "PIN VELOZ", new Date(), "DANILO@GMAIL.COM", "HABILITADO", 1, 1, 1));
         
+        //AdmPersonasDataBase.eliminar("1561651651");
+                
+        ArrayList<Persona> lista = AdmPersonasDataBase.consultar();
+        if(lista!=null && lista.size()>0){
+            for (Persona persona : lista) {
+                System.out.println(persona.toString());
+            }
+        }else{
+            System.out.println("lista nula");
+        }
     }
 }
