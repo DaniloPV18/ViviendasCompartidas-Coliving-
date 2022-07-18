ab29d1e82f60a3f88febfb3760d30d32f612287b
Danilo <damipive1809@gmail.com>
2022-07-18 02:03:13 -0500

    Métodos independientes Test

--- test/testController/TestAdmPersonas.java
+++ test/testController/TestAdmPersonas.java
@@ -16,14 +16,27 @@ import model.Persona;
  */
 public class TestAdmPersonas {
     public static void main(String[] args) {
+        insertarDatosPrueba();      
+        
+        consultarTabla();
+               
+        AdmPersonasDataBase.actualizar(44, new Persona("1561651651", "DANILO MIGUEL", "PIN VELOZ", new Date(), "DANILO@GMAIL.COM", "HABILITADO", 1, 1, 1));
+        
+        AdmPersonasDataBase.eliminar("1561651651");
+                
+        consultarTabla();
+    }
+
+    private static void insertarDatosPrueba() {
         AdmPersonasDataBase.insertar(new Persona("00000001", "MELANY PRUEBA", "PRUEBA PRUEBA", new Date(), "PRUEBA@GMAIL.COM", "HABILITADO", 1, 1, 1));
         AdmPersonasDataBase.insertar(new Persona("0931600548", "ANGEL MIGUEL", "PIGUAVE GUAGUA", new Date(), "ANGEL@GMAIL.COM", "HABILITADO", 1, 1, 1));
         AdmPersonasDataBase.insertar(new Persona("0984844848", "MICHAEL JOSEPH", "JOESTAR GUAGUA", new Date(), "MICHAEL@GMAIL.COM", "HABILITADO", 1, 1, 1));
         AdmPersonasDataBase.insertar(new Persona("0941848487", "ANGEL PALOMINO", "PIGUAVE GUAGUA", new Date(), "ANGEL@GMAIL.COM", "HABILITADO", 1, 1, 1));
         AdmPersonasDataBase.insertar(new Persona("0984816584", "JONATHAN PALOMINO", "JOESTAR GUAGUA", new Date(), "JONATHAN@GMAIL.COM", "HABILITADO", 1, 1, 1));
-        AdmPersonasDataBase.insertar(new Persona("0936878465", "JOTARO KOUJO", "PIGUAVE GUAGUA", new Date(), "JOTARO@GMAIL.COM", "HABILITADO", 1, 1, 1));        
-        
-        
+        AdmPersonasDataBase.insertar(new Persona("0936878465", "JOTARO KOUJO", "PIGUAVE GUAGUA", new Date(), "JOTARO@GMAIL.COM", "HABILITADO", 1, 1, 1));  
+    }
+
+    private static void consultarTabla() {
         ArrayList<Persona> ls1 = AdmPersonasDataBase.consultar();
         if(ls1!=null && ls1.size()>0){
             for (Persona persona : ls1) {
@@ -31,18 +44,6 @@ public class TestAdmPersonas {
             }
         }else{
             System.out.println("lista nula");
-        }        
-        AdmPersonasDataBase.actualizar(44, new Persona("1561651651", "DANILO MIGUEL", "PIN VELOZ", new Date(), "DANILO@GMAIL.COM", "HABILITADO", 1, 1, 1));
-        
-        AdmPersonasDataBase.eliminar("1561651651");
-                
-        ArrayList<Persona> lista = AdmPersonasDataBase.consultar();
-        if(lista!=null && lista.size()>0){
-            for (Persona persona : lista) {
-                System.out.println(persona.toString());
-            }
-        }else{
-            System.out.println("lista nula");
-        }
+        } 
     }
 }
