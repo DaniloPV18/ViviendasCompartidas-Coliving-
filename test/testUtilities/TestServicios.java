0b61d61cc06e9d23d5da74f327d269720e2bc6c6
Danilo <damipive1809@gmail.com>
2022-07-18 23:22:56 -0500

    Renombrar paquete

--- test/testUtilities/TestServicios.java
+++ test/testUtilities/TestServicios.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package TestUtilities;
+package testUtilities;
 
 import java.util.Date;
 import model.Persona;
